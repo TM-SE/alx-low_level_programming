bf550c79ce85410e95e898bbe9401317a1aa9fe5
Tashana Mina <tashanamina@gmail.com>
2023-07-13 19:47:04 +0000

    adding 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*main - entry point
+*Return: Always (0) SUCCESS
+*/
+int main(void)
+{
+char sentence[52];
+strcpy(sentence, "\"Programming is like building a multilingual puzzle");
+puts(sentence);
+return (0);
+
+}
+
