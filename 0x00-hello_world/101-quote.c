0d24af299ffeea5048a1e4fd38f278938acfa805
Tashana Mina <tashanamina@gmail.com>
2023-07-14 12:12:36 +0000

    adding 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - Entry point
+*function - write()
+*Return - 1
+*/
+int main(void)
+{
+	int sen;
+
+sen = write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+
+	return (sentence);
+}
