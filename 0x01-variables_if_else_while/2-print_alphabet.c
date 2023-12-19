a934e2b6efe4058afe51788c9aafa50c2b31b156
TM-SE <tashanamina@gmail.com>
2023-12-19 03:14:31 +0200

    adding  2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,11 @@ int main(void)
 {
 
 	char abc;
-	for( abc ='a'; abc <= 'z'; abc++)
-	{	
+	for (abc = 'a'; abc <= 'z'; abc++)
+
+	{
 	putchar(abc);
 	}
-	putchar('\n');
+	putchar ('\n');
 	return(0);
 }
