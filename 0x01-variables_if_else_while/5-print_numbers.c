29a4203cafef628859b7d606203c3463dace7673
TM-SE <tashanamina@gmail.com>
2023-12-19 11:45:55 +0200

    adding 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	int number;
+
+	while ( number < 10)
+	{
+		printf("%d number");
+	}
+	return (0);
+}
