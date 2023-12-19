c6afa3ebcd1fefba2b6d20e0bb8ebfa0d0686457
TM-SE <tashanamina@gmail.com>
2023-12-19 13:36:54 +0200

    adding corrctions

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,10 @@ int main(void)
 	int num;
 	char hex[] = "0123456789abcdef";
 
-	for ( num = 0; num < 16; num++)
+	for (num = 0; num < 16; num++)
 	{
 		putchar(hex[num]);
 	}
-		
 	putchar('\n');
 
 	return (0);
