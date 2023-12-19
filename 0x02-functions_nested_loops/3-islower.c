d51d7d13d6a0f163536a459cb32420012967d9cb
TM-SE <tashanamina@gmail.com>
2023-12-19 21:29:46 +0200

    adding  3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checking for lowercase char
+ *
+ * Return: return
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
