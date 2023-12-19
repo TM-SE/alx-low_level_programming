1af8507ab2a2a25b9e032026a5415132d776b0ed
TM-SE <tashanamina@gmail.com>
2023-12-19 20:36:59 +0200

    adding  1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main- entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		_putchar(abc);
+	}
+	_putchar('\n');
+}
