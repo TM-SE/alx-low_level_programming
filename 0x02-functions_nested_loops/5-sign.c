b7ca21095c8b3a7fab476a6fa55f5e0d478929f9
TM-SE <tashanamina@gmail.com>
2024-01-09 12:34:21 +0200

    adding corrections

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar('+');
+		_putchar(43);
 	}
 	else if (n == 0)
 	{
 		return (0);
-		_putchar('0');
+		_putchar(48);
 	}
 		return (-1);
-		_putchar('-');
+		_putchar(45);
 
 
 }
