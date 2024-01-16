105a05f1b8612da88f5597b15012ed48765a710b
TM-SE <tashanamina@gmail.com>
2024-01-16 17:46:13 +0200

    adding 7-last_didgit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_last_digit - funcction to print last digit
+*Return: Return the last digit
+*/
+int print_last_digit(int r)
+{
+	int lastd;
+
+	lastd = r % 10;
+
+	return(lastd);
+
+
+
+
+}
