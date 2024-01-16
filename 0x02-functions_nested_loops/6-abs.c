58fe21456b8d9e61efa27f32d2452b15c57494b7
TM-SE <tashanamina@gmail.com>
2024-01-16 17:17:30 +0200

    adding corrections to code and betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
-*int _abs - function that computes the absolute value of an integer
+*_abs - function that computes the absolute value of an integer
 *@r: variable to be checked
 *Return: returns asolute value
 */
 int _abs(int r)
 {
-	
-	if ( r < 0)
+
+	if (r < 0)
 	{
 		r = r * -1;
-		_putchar(r + '0');
 	}
+		return (r);
 
-	return(r);
 }
