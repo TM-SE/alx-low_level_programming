a252d07433e838b21f25d46875d353ace53d9ed5
TM-SE <tashanamina@gmail.com>
2024-01-17 13:38:40 +0200

    adding 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+*main -entry point
+*Return: Return (0)
+*/
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 8; num1++)
+	{
+	for (num2 = 0; num2 <= 9; num2++)
+	{
+	if (num1 == num2 || num1 > num2)
+	{
+		continue;
+	}
+		putchar(num1 + '0');
+		putchar(num2 + '0');
+	if (num1 != 8 || num2 != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+		return (0);
+}
