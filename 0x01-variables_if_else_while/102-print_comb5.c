bd6c88465692816533d9bf832c66084a254b4655
TM-SE <tashanamina@gmail.com>
2024-01-17 19:01:18 +0200

    adding prigram

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: return (0)
+*/
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+	for (num2 = 0; num2 <= 9; num2++)
+	{
+	for (num3 = 0; num3 <= 9; num3++)
+	{
+	for (num4 = 0; num4 <= 9; num4++)
+	{
+		putchar(num1 + '0');
+		putchar(num2 + '0');
+		putchar(' ');
+		putchar(num3 + '0');
+		putchar(num4 + '0');
+		putchar(',');
+
+	}
+	}
+	}
+	}
+		return (0);
+} 
