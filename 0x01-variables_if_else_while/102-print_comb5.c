ffeda203c7f4a2e831a7ce8b839cce74adb0d8b6
TM-SE <tashanamina@gmail.com>
2024-01-18 10:34:48 +0200

    adding  102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,16 +18,21 @@ int main(void)
 	{
 	for (num4 = 0; num4 <= 9; num4++)
 	{
+	if (num1 < num2 || num2 < num3)
+	{
 		putchar(num1 + '0');
 		putchar(num2 + '0');
 		putchar(' ');
 		putchar(num3 + '0');
 		putchar(num4 + '0');
+	if ((num1 != 9 || num2 != 8) || (num3 != 9 || num4 != 9))
+	{
 		putchar(',');
+	}
 
 	}
 	}
 	}
 	}
-		return (0);
-} 
+	}	return (0);
+}
